21136b6959e4ae20b6c4c9c86ccf0a6702739405
NhatNguyen5 <nhatnguyen2807@gmail.com>
2026-01-04 23:52:09 -0600

    M: Update .gitignore to ignore public/. Update main.sh to tells Python the project root is the import base. Update main.py to copy static/ into public/. Move index.css and image/tolkien.png to static/.
    .gitignore:
        Add public/
    main.sh:
        Update to tells Python the project root is the import base.
    src/
        main.py:
            Update main.py to copy all static/ into a cleaned public/
    static/
        image/
            Add tolkien.png
        index.css:
            Move from public/.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,29 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+from logging_module.my_logging import logger
 
 def main():
-    new_textnode = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(new_textnode)
+    if os.path.exists("public"):
+        shutil.rmtree("public")
+    os.mkdir("public")
+    copyDir("static", "public")
+
+def copyDir(source, destination):
+    log = logger()
+    log.enable = True
+    all_objects = os.listdir(source)
+    for object in all_objects:
+        log(f"Object: {object}")
+        src_path = os.path.join(source, object)
+        if os.path.isfile(src_path):
+            log(f"Copy file: {object}")
+            shutil.copy(src_path, destination)
+            continue
+        log(f"Copy dir: {object}")
+        dest_path = os.path.join(destination, object)
+        os.mkdir(dest_path)
+        copyDir(src_path, dest_path)
+    return 0
 
 if __name__ == "__main__":
     main()
